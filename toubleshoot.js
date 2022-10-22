8481b776ea11f9490c22ee25f6b7dec5f0ac5af4
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2022-10-22 17:20:52 -0400

    removed autoplay

---
+++ toubleshoot.js
@@ -0,0 +1,11 @@
+document.getElementById('videomp4')
+video.onloadeddata = function() {
+    console.log('videomp4 loaded :)')
+    alert("Browser has loaded the current frame");
+}
+
+document.getElementById('videowebm')
+video.onloadeddata = function() {
+    console.log('videowebm loaded :)')
+    alert("Browser has loaded the current frame");
+}
